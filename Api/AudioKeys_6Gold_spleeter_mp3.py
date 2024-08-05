cb45b448d5e594ece4dc292cf638ec6e41fa7c11
Fatemeh.V.Unesi <90507840+F-V-Younesi@users.noreply.github.com>
2024-08-05 07:37:14 -0700

    Update AudioKeys_6Gold_spleeter_mp3.py

--- Api/AudioKeys_6Gold_spleeter_mp3.py
+++ Api/AudioKeys_6Gold_spleeter_mp3.py
@@ -1,28 +1,3 @@
-import spleeter
-import moviepy.editor
-import io,os
-from pathlib import Path
-import select
-import subprocess as sp
-import sys
-from typing import Dict, Tuple, Optional, IO
-from pydub import AudioSegment, silence
-import librosa #, numpy as np
-from parsivar import SpellCheck
-import whisper
-import numpy as np
-import shutil
-import re
-def dummy_npwarn_decorator_factory():
-  def npwarn_decorator(x):
-    return x
-  return npwarn_decorator
-np._no_nep50_warning = getattr(np, '_no_nep50_warning', dummy_npwarn_decorator_factory)
-from perke.unsupervised.graph_based import TopicRank
-#from flask import Flask, render_template, request
-#from whispermodel import *
-
-
 #functions:
 #this function Investigate that the input file is audio or video or None of them:
 def is_video(path):
@@ -230,31 +205,13 @@ def predict(path,caption):
       os.rename(path,'data/Audio.mp3')
     if os.path.exists('data/Audio.mp3'):
       print('**Start of the operation to check the presence of music in the audio file:\n')
-      # !python3 -m pip install -U git+https://github.com/facebookresearch/demucs#egg=demucs
-      #model = "htdemucs"
-      #two_stems = None   # only separate one stems from the rest, for instance
-      # two_stems = "vocals"
-      # Options for the output audio:
-      #mp3 = True
-      #mp3_rate = 320
-      #float32 = False  # output as float 32 wavs, unsused if 'mp3' is True.
-      #int24 = False    # output as int24 wavs, unused if 'mp3' is True.
-      #in_path = '/content/demucs'
-      #out_path = '/content/demucs_separated/'
-      #remove_music()
-      # !zip -r separated.zip separated
+
       remove_music2()
       
       #AudioSegment.from_wav("/home/younesi/data/output/Audio/accompaniment.wav").export("/home/younesi/data/output/Audio/accompaniment.mp3", format="mp3")
       AudioSegment.from_wav("/home/younesi/data/output/Audio/vocals.wav").export("/home/younesi/data/output/Audio/vocals.mp3", format="mp3")
-
       overlay_music=AudioSegment.from_wav('/home/younesi/data/output/Audio/accompaniment.wav')
-      #music1=AudioSegment.from_mp3('data/separated/htdemucs/Audio/bass.mp3')
-      #music2=AudioSegment.from_mp3('data/separated/htdemucs/Audio/drums.mp3')
-      #music3=AudioSegment.from_mp3('data/separated/htdemucs/Audio/other.mp3')
       vocal=AudioSegment.from_mp3('/home/younesi/data/output/Audio/vocals.mp3')
-      #overlay_music = music1.overlay(music2, position=0).overlay(music3, position=0)
-      #overlay_music.export('data/music.mp3',format='mp3')
       useful_content=music_mode(overlay_music,'data/output/Audio/accompaniment.wav',vocal)
       
       audio_path='data/output/Audio/vocals.mp3'
