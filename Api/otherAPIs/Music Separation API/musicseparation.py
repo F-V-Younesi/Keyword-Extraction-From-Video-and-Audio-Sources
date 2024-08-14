8e41d01ff56b75bc9deca4e7d6930889bc97f38e
Fatemeh.V.Unesi <90507840+F-V-Younesi@users.noreply.github.com>
2024-08-14 06:42:14 -0700

    Update musicseparation.py

--- Api/otherAPIs/Music Separation API/musicseparation.py
+++ Api/otherAPIs/Music Separation API/musicseparation.py
@@ -1,10 +1,6 @@
 def remove_music2(path):
   os.system('spleeter separate -o output/ '+path)
-
+  return "Convertion Done!"
 def predict(path):
-      remove_music2(path)
-      filename=path.replace('static/','')
-      overlay_music=AudioSegment.from_wav('output/'+filename+'/accompaniment.wav')
-      vocal=AudioSegment.from_wav('output/'+filename+'/vocals.wav')
-      useful_content=music_mode(overlay_music,'output/'+filename+'/accompaniment.wav',vocal)
-      return useful_content
+      output=remove_music2(path)
+      return output
